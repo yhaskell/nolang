a91868be8dc95394acf2770fdf66d2c58313589f
Igor N. Dultsev <igor.dultsev@global-fashion-group.com>
2023-12-25 18:20:07 +0100

    run tokeniser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#![allow(dead_code)]
+
+mod source_code;
+mod tokeniser;
+
 fn main() {
-  println!("Hello, world!");
+  let code = "42++58+-+24+|2";
+
+  println!("{}", code);
+
+  let tokens = tokeniser::tokenise(code);
+  for token in tokens {
+    println!("{:?}", token.value);
+  }
 }
