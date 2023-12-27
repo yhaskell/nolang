902d67e1eb684b59870e43057c809a95e50d5529
Igor N. Dultsev <igor.dultsev@global-fashion-group.com>
2023-12-27 17:10:36 +0100

    refactor tests and finalise the api

--- src/tokeniser/test.rs
+++ src/tokeniser/test.rs
@@ -1,286 +1,174 @@
-macro_rules! assert_token_value {
-  ($t: expr, $v: expr) => {
-    assert_eq!($t.value, $v)
-  };
-}
+use super::{token::ErrorCode, token::TokenValue, tokenise};
 
+/// Returns Error token
+///
+/// # Examples
+///
+/// ```
+/// let token = error!("1failed%_token", ErrorCode::UnexpectedToken)
+/// ```
 macro_rules! error {
   ($v: expr, $t: expr) => {
-    crate::tokeniser::TokenValue::Error($v.to_string(), $t)
+    TokenValue::Error($v.to_string(), $t)
   };
 }
 
-mod identifier {
-  use crate::tokeniser::{token::ErrorCode, tokenise};
-
-  macro_rules! ident {
-    ($value: expr) => {
-      crate::tokeniser::TokenValue::Identifier($value.to_string())
-    };
-  }
-
-  #[test]
-  fn can_tokenise_one() {
-    let tokens = tokenise("test");
-    assert_eq!(tokens.len(), 1);
-
-    assert_token_value!(tokens[0], ident!("test"))
-  }
-
-  #[test]
-  fn can_tokenise_many() {
-    let tokens = tokenise("test success");
-    assert_eq!(tokens.len(), 2);
-
-    assert_token_value!(tokens[0], ident!("test"));
-    assert_token_value!(tokens[1], ident!("success"));
-  }
-
-  #[test]
-  fn errors_on_id_starting_with_number() {
-    let tokens = tokenise("1test");
-    assert_eq!(tokens.len(), 1);
-
-    assert_token_value!(tokens[0], error!("1test", ErrorCode::UnexpectedToken))
-  }
-}
-
-mod char_literal {
-  use crate::tokeniser::{
-    token::{ErrorCode, TokenValue},
-    tokenise,
+/// Returns Identifier token
+///
+/// # Examples
+/// ```
+/// let token = ident!("test")
+/// ```
+macro_rules! ident {
+  ($value: expr) => {
+    TokenValue::Identifier($value.to_string())
   };
-
-  macro_rules! char {
-    ($c: expr) => {
-      TokenValue::CharLiteral($c)
-    };
-  }
-
-  #[test]
-  fn can_tokenise_simple() {
-    let tokens = tokenise("'c'");
-
-    assert_eq!(tokens.len(), 1);
-
-    assert_token_value!(tokens[0], char!('c'))
-  }
-
-  #[test]
-  fn can_tokenise_many() {
-    let tokens = tokenise("'c' '\\n' '\\u0041'");
-    assert_eq!(tokens.len(), 3);
-
-    assert_token_value!(tokens[0], char!('c'));
-    assert_token_value!(tokens[1], char!('\n'));
-    assert_token_value!(tokens[2], char!('A'));
-  }
-
-  #[test]
-  fn can_tokenise_escape() {
-    let tokens = tokenise("'\\n'");
-
-    assert_eq!(tokens.len(), 1);
-
-    assert_token_value!(tokens[0], char!('\n'))
-  }
-
-  #[test]
-  fn errors_when_wrong_escape() {
-    let tokens = tokenise("'\\a'");
-    assert_token_value!(tokens[0], error!("'\\a'", ErrorCode::UnknownEscapeSequence))
-  }
-
-  #[test]
-  fn can_tokenise_unicode() {
-    let tokens = tokenise("'試' 'é'");
-
-    assert_token_value!(tokens[0], char!('試'));
-    assert_token_value!(tokens[1], char!('é'));
-  }
-
-  #[test]
-  fn can_tokenise_unicode_escape() {
-    let tokens = tokenise("'\\u0041'");
-    assert_token_value!(tokens[0], char!('A'))
-  }
 }
 
-mod bracket {
-  use crate::tokeniser::tokenise;
-
-  macro_rules! bracket {
-    ($value: expr) => {
-      crate::tokeniser::TokenValue::Bracket($value)
-    };
-  }
-
-  #[test]
-  fn can_tokenise_one() {
-    let tokens = tokenise("(");
-    assert_token_value!(tokens[0], bracket!('('))
-  }
-}
-
-mod string_literal {
-  use crate::tokeniser::{
-    token::{ErrorCode, TokenValue},
-    tokenise,
+/// Returns Bracket token
+///
+/// # Examples
+/// ```
+/// let token = bracket!('(')
+/// ```
+macro_rules! bracket {
+  ($value: expr) => {
+    TokenValue::Bracket($value)
   };
-
-  macro_rules! string {
-    ($c: expr) => {
-      TokenValue::StringLiteral($c.to_string())
-    };
-  }
-
-  #[test]
-  fn can_tokenise_simple() {
-    let tokens = tokenise("\"test\"");
-
-    assert_token_value!(tokens[0], string!("test"));
-  }
-
-  #[test]
-  fn errors_on_unfinished_string() {
-    let tokens = tokenise("\"test");
-
-    assert_token_value!(tokens[0], error!("\"test", ErrorCode::UnterminatedStringLiteral));
-  }
-
-  #[test]
-  fn errors_on_string_newline() {
-    let tokens = tokenise("\"test\n\"test\"");
-
-    assert_token_value!(tokens[0], error!("\"test", ErrorCode::UnterminatedStringLiteral));
-
-    assert_token_value!(tokens[1], string!("test"))
-  }
-
-  #[test]
-  fn can_tokenise_multiline_string() {
-    let tokens = tokenise("\"\"\"test\ntest\"\"\"");
-
-    assert_token_value!(tokens[0], string!("test\ntest"))
-  }
-
-  #[test]
-  fn can_tokenise_string_with_escape_sequences() {
-    let tokens = tokenise("\"\\n\\t\\u0041\"");
-
-    assert_token_value!(tokens[0], string!("\n\tA"))
-  }
 }
 
-mod number_or_dot {
-  use crate::tokeniser::{tokenise, TokenValue};
-
-  macro_rules! int {
-    ($c: expr) => {
-      TokenValue::IntLiteral($c)
-    };
-  }
-  macro_rules! float {
-    ($c: expr) => {
-      TokenValue::FloatLiteral($c)
-    };
-  }
-
-  #[test]
-  fn can_consume_0() {
-    let tokens = tokenise("0");
-
-    assert_token_value!(tokens[0], int!(0))
-  }
-
-  #[test]
-  fn can_consume_decimal() {
-    let tokens = tokenise("42");
-
-    assert_token_value!(tokens[0], int!(42))
-  }
-
-  #[test]
-  fn can_consume_very_long() {
-    let tokens = tokenise("340282366920938463463374607431768211455");
-
-    assert_token_value!(tokens[0], int!(340282366920938463463374607431768211455))
-  }
-
-  #[test]
-  fn errors_on_too_large_number() {
-    let tokens = tokenise("340282366920938463463374607431768211456");
-
-    assert_token_value!(
-      tokens[0],
-      error!(
-        "340282366920938463463374607431768211456",
-        crate::tokeniser::token::ErrorCode::IntLiteralTooLong
-      )
-    )
-  }
-
-  #[test]
-  fn can_consume_octal() {
-    let tokens = tokenise("0100");
-    assert_token_value!(tokens[0], int!(64))
-  }
-
-  #[test]
-  fn can_consume_hex_0() {
-    let tokens = tokenise("0x0");
-
-    assert_token_value!(tokens[0], int!(0))
-  }
-
-  #[test]
-  fn can_consume_hex() {
-    let tokens = tokenise("0x7f");
-
-    assert_token_value!(tokens[0], int!(127));
-  }
-
-  #[test]
-  fn can_consume_float() {
-    let tokens = tokenise("7.5");
-
-    assert_token_value!(tokens[0], float!(7.5));
-  }
-
-  #[test]
-  fn can_consume_float_with_no_int_part() {
-    let tokens = tokenise(".45");
-
-    assert_token_value!(tokens[0], float!(0.45));
-  }
-
-  #[test]
-  fn can_consume_float_with_no_float_part() {
-    let tokens = tokenise("42.");
-
-    assert_token_value!(tokens[0], float!(42.0));
-  }
-
-  #[test]
-  fn can_consume_dot() {
-    let tokens = tokenise(".");
-
-    assert_token_value!(tokens[0], TokenValue::Operator(".".to_string()))
-  }
-
-  #[test]
-  fn can_consume_multiple() {
-    let tokens = tokenise("5 7");
-
-    assert_token_value!(tokens[0], int!(5));
-    assert_token_value!(tokens[1], int!(7));
-  }
-
-  #[test]
-  fn can_consume_numbers_with_operators() {
-    let tokens = tokenise("5+7");
-
-    assert_token_value!(tokens[0], int!(5));
-    assert_token_value!(tokens[2], int!(7));
-  }
+/// Returns Char Literal token
+///
+/// # Examples
+/// ```
+/// let token = char!('ü')
+/// ```
+macro_rules! char {
+  ($c: expr) => {
+    TokenValue::CharLiteral($c)
+  };
 }
+
+/// Returns Operator token
+///
+/// # Examples
+/// ```
+/// let token = operator!("::")
+/// ```
+macro_rules! operator {
+  ($value: expr) => {
+    TokenValue::Operator($value.to_string())
+  };
+}
+
+/// Returns String Literal token
+///
+/// # Examples
+/// ```
+/// let token = string!("test")
+/// ```
+macro_rules! string {
+  ($c: expr) => {
+    TokenValue::StringLiteral($c.to_string())
+  };
+}
+
+/// Returns Int Literal token
+///
+/// # Examples
+/// ```
+/// let token = int!(174)
+/// ```
+macro_rules! int {
+  ($c: expr) => {
+    TokenValue::IntLiteral($c)
+  };
+}
+
+/// Returns Float Literal token
+///
+/// # Examples
+/// ```
+/// let token = float!(7.45)
+/// ```
+macro_rules! float {
+  ($c: expr) => {
+    TokenValue::FloatLiteral($c)
+  };
+}
+
+macro_rules! test {
+  ( $name: ident, $code: expr, { $($index: expr => $token: expr),* } ) => {
+    #[test]
+    fn $name() {
+      let tokens = tokenise($code);
+
+      $(
+      assert_eq!(tokens[$index].value, $token);
+      )*
+    }
+  };
+  ( $name: ident, $code: expr, $len: expr, { $($index: expr => $token: expr),* } ) => {
+    #[test]
+    fn $name() {
+      let tokens = tokenise($code);
+
+      assert_eq!($len, tokens.len());
+
+      $(
+      assert_eq!(tokens[$index].value, $token);
+      )*
+    }
+  };
+}
+
+test!(identifier_single, "test", { 0 => ident!("test") });
+test!(identifier_multiple, "test success", 2, { 0 => ident!("test"), 1 => ident!("success") });
+test!(identifier_with_error, "1test", { 0 => error!("1test", ErrorCode::UnexpectedToken) });
+
+test!(char_single, "'c'", { 0 => char!('c') });
+test!(char_multiple, r"'c' '\n' '\u0041'", 3, { 0 => char!('c'), 1 => char!('\n'), 2 => char!('A') });
+test!(char_with_escape_sequence, r"'\n'", { 0 => char!('\n') });
+test!(char_with_wrong_escape_sequence, r"'\a'", { 0 => error!(r"'\a'", ErrorCode::UnknownEscapeSequence) });
+test!(char_with_unicode_symbol, "'試' 'é'", { 0 => char!('試'), 1 => char!('é') });
+test!(char_with_unicode_escape_sequence, r"'\u0041'", { 0 => char!('A') });
+
+test!(bracket_single, "(", { 0 => bracket!('(') });
+test!(bracket_multiple, "()", 2,  { 0 => bracket!('('), 1 => bracket!(')') });
+
+test!(string_single, r#""test""#, { 0 => string!("test") });
+test!(string_unfinished_with_eof, r#""test"#, { 0 => error!("\"test", ErrorCode::UnterminatedStringLiteral) });
+test!(string_unfinished_with_newline, "\"test\n\"test\"", 2, { 0 => error!("\"test", ErrorCode::UnterminatedStringLiteral), 1 => string!("test") });
+test!(string_multiple, &r#""""test test""""#.replace(' ', "\n"), { 0 => string!("test\ntest") });
+test!(string_with_escape_sequences, r#""\n\t\u0041""#, { 0 => string!("\n\tA") });
+
+test!(int_decimal_zero, "0", { 0 => int!(0) });
+test!(int_decimal_single, "42", { 0 => int!(42) });
+test!(int_very_long, "340282366920938463463374607431768211455", { 0 => int!(340282366920938463463374607431768211455) });
+test!(int_too_long_error, "340282366920938463463374607431768211456", { 0 => error!("340282366920938463463374607431768211456", ErrorCode::IntLiteralTooLong) });
+test!(int_octal, "0100", { 0 => int!(64) });
+test!(int_hex_zero, "0x0", { 0 => int!(0) });
+test!(int_hex, "0x7f", { 0 => int!(127) });
+test!(int_multiple, "5 7", {0 =>  int!(5), 1 => int!(7) });
+test!(int_divided_by_operators, "5+7", { 0 => int!(5), 2 => int!(7) });
+
+test!(float_single, "7123.5514", { 0 => float!(7123.5514) });
+test!(float_without_integer_part, ".45", { 0 => float!(0.45) });
+test!(float_without_floating_part, "42.", { 0 => float!(42.0) });
+
+test!(operator_dot, ".", { 0 => operator!(".") });
+test!(operator_single_char, "%", { 0 => operator!("%")});
+test!(operator_multiple_chars, "::", { 0 => operator!("::") });
+test!(operator_multiple, "++::--", { 0 => operator!("++"), 1 => operator!("::"), 2 => operator!("--") });
+test!(combined, "++test * (a-b).c", {
+  0 => operator!("++"),
+  1 => ident!("test"),
+  2 => operator!("*"),
+  3 => bracket!('('),
+  4 => ident!("a"),
+  5 => operator!("-"),
+  6 => ident!("b"),
+  7 => bracket!(')'),
+  8 => operator!("."),
+  9 => ident!("c")
+});
