4ceabd83158feefce0b0dcca22f7c845c165b730
Igor N. Dultsev <igor.dultsev@global-fashion-group.com>
2023-12-30 14:17:14 +0100

    process computations with assignments

--- src/computer/mod.rs
+++ src/computer/mod.rs
@@ -1,45 +1,80 @@
+use std::collections::HashMap;
+
 use crate::ast::{Ast, Literal, Value};
 
-pub fn compute(ast: &Ast) -> Result<f64, String> {
-  match &ast.value {
-    Value::Empty => Err(format!("Cannot compute empty value")),
-    Value::Literal(l) => match l {
-      Literal::Char(_) => Err(format!("Char literals are currently not supported")),
-      Literal::Int(i) => {
-        if let Ok(u) = i32::try_from(*i).and_then(|i| Ok(f64::from(i))) {
-          Ok(u)
-        } else {
-          Err(format!("Error: {} is too big", i))
+pub struct Computer {
+  context: HashMap<String, f64>,
+}
+
+impl Computer {
+  pub fn new() -> Self {
+    Computer {
+      context: HashMap::new(),
+    }
+  }
+
+  pub fn iter(&self) -> std::collections::hash_map::Iter<'_, String, f64> {
+    self.context.iter()
+  }
+
+  pub fn compute(&mut self, ast: &Ast) -> Result<f64, String> {
+    match &ast.value {
+      Value::Empty => Err(format!("Cannot compute empty value")),
+      Value::Literal(l) => match l {
+        Literal::Char(_) => Err(format!("Char literals are currently not supported")),
+        Literal::Int(i) => {
+          if let Ok(u) = i32::try_from(*i).and_then(|i| Ok(f64::from(i))) {
+            Ok(u)
+          } else {
+            Err(format!("Error: {} is too big", i))
+          }
+        }
+        Literal::Float(f) => Ok(*f),
+        Literal::String(_) => Err(format!("String literals are currently not supported")),
+      },
+      Value::Identifier(id) => match self.context.get(id) {
+        Some(value) => Ok(*value),
+        None => Err(format!("{}: variable not found", id)),
+      },
+      Value::Unary(op, expr) => {
+        let value = self.compute(expr)?;
+
+        match op.as_str() {
+          "+" => Ok(value),
+          "-" => Ok(-value),
+          other => Err(format!("{}: unknown operator", other)),
         }
       }
-      Literal::Float(f) => Ok(*f),
-      Literal::String(_) => Err(format!("String literals are currently not supported")),
-    },
-    Value::Identifier(_) => Err(format!("Identifiers are currently not supported")),
-    Value::Unary(op, expr) => {
-      let value = compute(expr)?;
+      Value::Binary(left, op, right) => {
+        let left = self.compute(left)?;
+        let right = self.compute(right)?;
 
-      match op.as_str() {
-        "+" => Ok(value),
-        "-" => Ok(-value),
-        other => Err(format!("{}: unknown operator", other)),
+        match op.as_str() {
+          "+" => Ok(left + right),
+          "-" => Ok(left - right),
+          "*" => Ok(left * right),
+          "/" => Ok(left / right),
+          "%" => Ok(left % right),
+          other => Err(format!("{}: unknown operator", other)),
+        }
+      }
+      Value::Error(e) => Err(format!("{}", e)),
+      Value::Expression(e) => self.compute(e),
+      Value::Assignment(id, expr) => {
+        let value = self.compute(&expr);
+        let id = match &id.value {
+          Value::Identifier(id) => id.clone(),
+          _ => panic!("Should be assignment"),
+        };
+
+        match value {
+          Ok(value) => {
+            self.context.insert(id, value);
+            Ok(value)
+          }
+          error => error,
+        }
       }
     }
-    Value::Binary(left, op, right) => {
-      let left = compute(left)?;
-      let right = compute(right)?;
-
-      match op.as_str() {
-        "+" => Ok(left + right),
-        "-" => Ok(left - right),
-        "*" => Ok(left * right),
-        "/" => Ok(left / right),
-        "%" => Ok(left % right),
-        other => Err(format!("{}: unknown operator", other)),
-      }
-    }
-    Value::Error(e) => Err(format!("{}", e)),
-    Value::Expression(e) => compute(e),
-    Value::Assignment(_, _) => Err(format!("Assignments are currently not supported")),
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 use std::io::{stdin, stdout, Write};
 
-use computer::compute;
+use computer::Computer;
 
 mod ast;
 mod computer;
@@ -10,26 +10,39 @@ mod source_code;
 mod tokeniser;
 
 fn main() {
-  println!("Enter the expression (q to exit)");
+  println!("Enter the expression (l or list to list all variables, q or quit to exit)");
+
+  let mut computer = Computer::new();
 
   loop {
     print!("> ");
     stdout().flush().unwrap();
 
     let mut code = String::new();
-    stdin().read_line(&mut code).expect("Cannot read from stdin, exiting");
+    let res = stdin().read_line(&mut code).expect("Cannot read from stdin, exiting");
+
+    if res == 0 {
+      break;
+    }
 
     let code = code.trim();
 
-    if code == "q" {
-      return;
+    if code.len() < 1 {
+      continue;
+    } else if code == "q" || code == "quit" {
+      break;
+    } else if code == "l" || code == "list" {
+      for (k, v) in computer.iter() {
+        println!("{} = {}", k, v)
+      }
+      continue;
     }
 
     let ast = ast::from_string(code);
-    let result = compute(&ast);
+    let result = computer.compute(&ast);
 
     match result {
-      Ok(result) => println!("{} = {}", code, result),
+      Ok(result) => println!("{}", result),
       Err(e) => println!("Error: {}", e),
     };
   }
